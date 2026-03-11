f256407cc88ff576e9146602879f9ef1e6d5b43f
James <Jamestp19@gmail.com>
2026-03-11 18:20:13 -0400

    Add Sender interface for logging functionality

---
+++ src/main/java/edu/harvard/dbmi/avillach/logging/Sender.java
@@ -0,0 +1,8 @@
+package edu.harvard.dbmi.avillach.logging;
+
+import java.net.URI;
+
+interface Sender {
+    void send(byte[] body, URI auditEndpoint, LoggingClientConfig config,
+              LoggingEvent resolved, String bearerToken, String requestId);
+}
